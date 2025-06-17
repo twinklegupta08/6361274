3a3b60dc3d1d9545c3ba1caebdd227bb005ed92b
twinklegupta08 <149895743+twinklegupta08@users.noreply.github.com>
2025-06-18 00:47:13 +0530

    Excercise 1 done!

---
+++ Week _1/Design_principles/Excercise_1/Logger.java
@@ -0,0 +1,22 @@
+package Design_principles.Excercise_1;
+public class Logger {
+    private static Logger instance;
+
+    // Private constructor to prevent instantiation from other classes
+    private Logger() {
+        System.out.println("Logger instance created.");
+    }
+
+    // Public static method to provide access to the instance
+    public static Logger getInstance() {
+        if (instance == null) {
+            instance = new Logger(); // Lazy initialization
+        }
+        return instance;
+    }
+
+    // Example method to simulate logging
+    public void log(String message) {
+        System.out.println("LOG: " + message);
+    }
+}
